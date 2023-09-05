606ddde1db010422a20f4c10ef8a193513ed2233
Ivan Josipovic <9521987+IvanJosipovic@users.noreply.github.com>
2023-09-04 19:55:52 -0700

    wip

---
+++ src/BlazorInteropGenerator/BlazorInteropGenerator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace BlazorInteropGenerator
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct)]
+    public class BlazorInteropGeneratorAttribute : Attribute
+    {
+        private string Name;
+
+        /// <summary>
+        /// Generates C# Interfaces from TypeScript Definitions
+        /// </summary>
+        /// <param name="name">NPM Package or path to TS Definition</param>
+        public BlazorInteropGeneratorAttribute(string name)
+        {
+            Name = name;
+        }
+    }
+}
---
+++ tests/BlazorInteropGenerator.Tests/Tests.cs
@@ -0,0 +1,47 @@
+﻿using BlazorInteropGenerator;
+using Microsoft;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.Text;
+using Microsoft.VisualStudio.TestPlatform.Common.Utilities;
+using System.Collections.Immutable;
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Text;
+using Xunit;
+
+namespace BlazorInterpGenerator.Tests;
+
+public class Tests
+{
+    [Fact]
+    public void SimpleGeneratorTest()
+    {
+        string userSource = """
+namespace MyCode
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
+""";
+        var compilation = CreateCompilation(userSource);
+
+        var driver = CSharpGeneratorDriver.Create(new IIncrementalGenerator[] { new Generator() });
+        driver.RunGeneratorsAndUpdateCompilation(compilation, out var updatedCompilation, out var diagnostics);
+
+        Assert.Empty(diagnostics);
+        Assert.Empty(updatedCompilation.GetDiagnostics());
+    }
+
+    private static Compilation CreateCompilation(string source) => CSharpCompilation.Create(
+        assemblyName: "compilation",
+        syntaxTrees: new[] { CSharpSyntaxTree.ParseText(source, new CSharpParseOptions(LanguageVersion.Preview)) },
+        references: new[] { MetadataReference.CreateFromFile(typeof(Binder).GetTypeInfo().Assembly.Location) },
+        options: new CSharpCompilationOptions(OutputKind.ConsoleApplication)
+    );
+}
