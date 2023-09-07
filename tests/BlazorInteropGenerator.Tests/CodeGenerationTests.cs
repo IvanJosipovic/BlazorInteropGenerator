3ef0db7f8cef586273d3d4349955086dd9dc6217
Ivan Josipovic <9521987+IvanJosipovic@users.noreply.github.com>
2023-09-06 21:28:53 -0700

    feat: support boolean

--- tests/BlazorInteropGenerator.Tests/CodeGenerationTests.cs
+++ tests/BlazorInteropGenerator.Tests/CodeGenerationTests.cs
@@ -179,6 +179,38 @@ public class CodeGenerationTests
     }
 
     [Fact]
+    public void InterfacePropertyBoolean()
+    {
+        var tsd = """
+                export interface SomeType {
+                  prop1: boolean;
+                }
+                """;
+
+        var syntaxFactory = Generator.GenerateObjects(tsd, "SomeType", TSDParser.Enums.SyntaxKind.InterfaceDeclaration, "Test");
+
+        var code = syntaxFactory
+           .NormalizeWhitespace()
+           .ToFullString();
+
+        syntaxFactory.Members.Count.Should().Be(1);
+        var @interface = (syntaxFactory.Members[0] as NamespaceDeclarationSyntax).Members[0] as InterfaceDeclarationSyntax;
+
+        var prop1 = @interface.Members[0] as PropertyDeclarationSyntax;
+
+        prop1.Modifiers.Count.Should().Be(1);
+        prop1.Modifiers[0].Value.Should().Be("public");
+
+        prop1.Type.As<PredefinedTypeSyntax>().Keyword.Text.Should().Be("bool");
+
+        prop1.Identifier.Text.Should().Be("prop1");
+
+        prop1.AccessorList.Accessors.Count.Should().Be(2);
+        prop1.AccessorList.Accessors[0].Kind().Should().Be(SyntaxKind.GetAccessorDeclaration);
+        prop1.AccessorList.Accessors[1].Kind().Should().Be(SyntaxKind.SetAccessorDeclaration);
+    }
+
+    [Fact]
     public void InterfaceMethodVoid()
     {
         var tsd = """
